9dbbae140051b87e3b179a65504d8f32692d1da5
Anuragsingh2002 <singhanurag31july@gmail.com>
2025-02-18 22:35:37 +0530

    mlflow compeleted

--- src/cnnClassifier/pipeline/stage_04_model_evaluation.py
+++ src/cnnClassifier/pipeline/stage_04_model_evaluation.py
@@ -3,19 +3,19 @@ from src.cnnClassifier.components.prepare_base_model import PrepareBaseModel
 from src.cnnClassifier import logger
 
 
-STAGE_NAME = "Prepare base model"
+STAGE_NAME = "Evaluation stage"
 
 
-class PrepareBaseModelTrainingPipeline:
+class EvaluationPipeline:
     def __init__(self):
         pass
 
     def main(self):
-        config = ConfigurationManager()
-        prepare_base_model_config = config.get_prepare_base_model_config()
-        prepare_base_model = PrepareBaseModel(config=prepare_base_model_config)
-        prepare_base_model.get_base_model()
-        prepare_base_model.update_base_model()
+          config = ConfigurationManager()
+    eval_config = config.get_evaluation_config()
+    evaluation = Evaluation(eval_config)
+    evaluation.evaluation()
+    evaluation.log_into_mlflow()
 
 
     
@@ -23,7 +23,7 @@ if __name__ == '__main__':
     try:
         logger.info(f"*******************")
         logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
-        obj = PrepareBaseModelTrainingPipeline()
+        obj = EvaluationPipeline()
         obj.main()
         logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
     except Exception as e:
